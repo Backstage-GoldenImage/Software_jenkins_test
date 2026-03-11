74c5882b979730a2c2abef42fce04a18f7cf0e89
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-03-11 21:08:39 +0530

    Update welcome message in server response

--- template/server.js
+++ template/server.js
@@ -2,11 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Welcome to Backstage Self Service Portal!\n");
+  res.end("Welcome to Backstage SSP!\n");
 });
 
 const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
